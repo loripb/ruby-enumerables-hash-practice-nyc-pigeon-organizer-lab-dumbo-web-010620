f8bd52efda226b7928493074a083e0696e35e766
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-28 09:25:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,19 +4,22 @@ def nyc_pigeon_organizer(data)
   names = data[:gender][:male] + data[:gender][:female]
   result = {}
 
-  attributes = []
+  color_from_original_data = data[:color]
   value = []
 
   names.map do |bird|
-    result[bird] = {}
+    result[bird] = {
+      :color => attribute_check(bird, color_from_original_data)  
+    }
   end
   pp result
 end
 
 def attribute_check(bird, hash)
+  result = []
   hash.each_pair do |pair|
     if pair[1].include?(bird)
-      return pair[0]
+      result << pair[0]
     end
   end
 end
