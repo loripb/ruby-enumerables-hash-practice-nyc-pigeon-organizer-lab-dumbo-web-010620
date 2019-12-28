f40aa7625ccdae43887c922d256165ccfffc25ca
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-28 09:20:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,14 @@ def nyc_pigeon_organizer(data)
 
   names.map do |bird|
     result[bird] = {}
-
   end
   pp result
+end
+
+def attribute_check(bird, hash)
+  hash.each_pair do |pair|
+    if pair[1].include?(bird)
+      return pair[0]
+    end
+  end
 end
