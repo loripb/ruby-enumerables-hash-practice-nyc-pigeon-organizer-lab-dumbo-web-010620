495091ef31b63325bd6ee1279c83819e6447e94b
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-28 08:29:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pp'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  names = data[:gender][:male] + data[:gender][:female]
+  result = {}
+
+  attributes = []
+  value = []
+
+  names.map do |bird|
+
+    data.reduce({}) do |x, (cgl, value)|
+        result[bird] = cgl
+    end
+
+  end
+  pp result
 end
